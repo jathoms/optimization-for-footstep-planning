cf37b76a76115ac22dc9b0e27c70d6438f829f1a
james <james.thomson019@gmail.com>
2023-01-06 14:33:51 +0000

    minor adjustments, reachable_distance is now accurate and get_constraints is more generalised to work with create_environment in main

--- biped.py
+++ biped.py
@@ -4,11 +4,12 @@ import numpy as np
 from scipy.spatial import ConvexHull
 from sklearn import cluster
 
-m = 10  # number of wanted regions
+m = 16  # number of wanted regions
 n = 2  # dimension of space
-steps = 15  # number of steps in solution
+steps = 29  # number of steps in solution
 first_foot_forward = 'left'
-min_foot_separation_h = 0.01
+min_foot_separation_h = 0.1
+reachable_distance = 1.7
 
 rng = np.random.default_rng(seed=70)
 # m * 50 random points in n-D
@@ -25,33 +26,34 @@ while m != len(u_labels):
 obj_coords = points[15]
 hulls = [ConvexHull(points[label == i]) for i in u_labels]
 M = 1000
-reachable_distance = 0.1
 
 
-def get_constraints(model: gp.Model, all_hulls=hulls):
+def get_constraints(model: gp.Model, all_hulls=hulls, start=points[0], end=obj_coords, no_regions=m):
     all_constrs = np.array(
         [np.array([coords[:n] for coords in hull.equations]) for hull in all_hulls], dtype=object)
     all_rhs = np.array(
         [np.array([coords[-1] for coords in hull.equations]) for hull in all_hulls], dtype=object)
     A = all_constrs
     b = all_rhs
+    for hull in all_hulls:
+        print(hull.equations)
 
     x = model.addMVar((steps, n), lb=-gp.GRB.INFINITY, ub=gp.GRB.INFINITY)
     dists = model.addMVar((steps, n), ub=gp.GRB.INFINITY, lb=-gp.GRB.INFINITY)
-    active = model.addVars(steps, m, vtype=gp.GRB.BINARY)
+    active = model.addVars(steps, no_regions, vtype=gp.GRB.BINARY)
 
     rhs = [[] for _ in range(steps)]
     for i in range(steps):
         for val in b:
             rhs[i].append(model.addMVar(val.shape, lb=-
                                         gp.GRB.INFINITY, ub=gp.GRB.INFINITY))
-        model.addConstr(sum([active[i, j] for j in range(m)]) == 1)
+        model.addConstr(sum([active[i, j] for j in range(no_regions)]) == 1)
 
-    model.addConstr(x[-1] == obj_coords)
-    model.addConstr(x[0] == points[0])
+    model.addConstr(x[-1] == end)
+    model.addConstr(x[0] == start)
 
     # bipedality constraint.
-    if first_foot_forward == 'left':
+    if first_foot_forward == 'right':
         model.addConstrs(x[i][0] <= x[i+1][0] -
                          min_foot_separation_h for i in range(0, steps-1, 2))
         model.addConstrs(x[i+1][0] >= x[i+2][0] +
@@ -59,23 +61,23 @@ def get_constraints(model: gp.Model, all_hulls=hulls):
     else:
         model.addConstrs(x[i][0] >= x[i+1][0] +
                          min_foot_separation_h for i in range(0, steps-1, 2))
-        model.addConstrs(x[i][0] <= x[i+1][0] -
-                         min_foot_separation_h for i in range(1, steps-2, 2))
+        model.addConstrs(x[i+1][0] <= x[i+2][0] -
+                         min_foot_separation_h for i in range(0, steps-2, 2))
     for i in range(steps - 1):
         model.addConstr(dists[i] == (x[i] - x[i+1]))
-        model.addConstr(dists[i] @ dists[i] <= reachable_distance)
+        model.addConstr(dists[i] @ dists[i] <= reachable_distance**2)
         # model.addConstr(dists[i] @ dists[i] >= foot_size)
-        for j in range(m):
+        for j in range(no_regions):
             for k in range(len(b[j])):
                 model.addConstr(
                     rhs[i][j][k] == -(b[j][k]) + ((1 - active[i, j]) * M))
             model.addConstr(A[j] @ x[i] <= rhs[i][j])
     model.optimize()
-    plt.plot(obj_coords[0], obj_coords[1], marker="o", markersize=10, markeredgecolor="red", markerfacecolor="red",
+    plt.plot(end[0], end[1], marker="o", markersize=10, markeredgecolor="red", markerfacecolor="red",
              alpha=0.5)
-    plt.plot(points[0][0], points[0][1], marker="o", markersize=10, markeredgecolor="green", markerfacecolor="green",
+    plt.plot(start[0], start[1], marker="o", markersize=10, markeredgecolor="green", markerfacecolor="green",
              alpha=0.5)
-    print("Start Point:", points[0], "\nEnd Point:", obj_coords)
+    print("Start Point:", start, "\nEnd Point:", end)
     try:
         for idx, point in enumerate(x):
             if idx % 2 == 0:
@@ -103,8 +105,6 @@ def plot_hull(hull_vertices):
             plt.plot(vertices_[simplex, 0], vertices_[simplex, 1], 'k-')
     return
 
-
-model = gp.Model('first')
 
 # plot_hull(points)
 # get_constraints(model, hulls)
--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-import create_environment
+from create_environment import createRect, createSquare
 import biped
 import matplotlib.pyplot as plt
+import gurobipy as gp
+import numpy as np
 
-region_centers = [[0, 0], [2, 0], [4, 0], [6, 0], [6, 3], [4, 3],
-                  [2, 3], [0, 3], [0, 6], [2, 6], [4, 6], [6, 6], [6, 9]]
+region_centers = np.array([[0, 0], [2, 0], [4, 0], [6, 0], [6, 2], [6, 4], [4, 4],
+                           [2, 4], [0, 4], [0, 6], [0, 8], [2, 8], [4, 8], [6, 8], [6, 10], [6, 12]])
+
+hulls = []
+
 for center in region_centers:
-    create_environment.createSquare(center, 0.5)
+    hulls.append(createSquare(center, 0.5))
+
+model = gp.Model('optimizer')
+biped.get_constraints(
+    model, hulls, start=region_centers[0], end=region_centers[-1], no_regions=len(region_centers))
+
 plt.axis('scaled')
 plt.show()
